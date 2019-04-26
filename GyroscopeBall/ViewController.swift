1c03e5c1cf4d037db3bb263db07ad29ccf846f86
Art Karma <inste@icloud.com>
2019-04-26 19:28:37 +0300

    Initial Commit

---
+++ GyroscopeBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GyroscopeBall
+//
+//  Created by Art Karma on 4/26/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
